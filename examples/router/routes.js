bfe19c8bd0eb3e43296bb218a33bf000a3907d8d
cfgtt <1239832360@qq.com>
2019-08-28 11:28:44 +0800

    适应eslint

--- examples/router/routes.js
+++ examples/router/routes.js
@@ -1,48 +1,57 @@
 // 由于懒加载页面太多的话会造成webpack热更新太慢，所以开发环境不使用懒加载，只有生产环境使用懒加载
-const _import = require('ex/libs/util.import.' + process.env.NODE_ENV)
+// eslint-disable-next-line no-underscore-dangle
+const _import = require('ex/libs/util.import.' + process.env.NODE_ENV);
 
 
-import layout from 'ex/layout'
+import layout from 'ex/layout';
 
 
 /**
  * 在主框架内显示
  */
 const frameIn = [
-        {
-                path: '/index',
+    {
+        path: '/',
+        name: '',
+        component: layout,
+        children: [
+            {
+                path: '',
+                name: '',
+                meta: {
+                    auth: true
+                },
+                component: _import('system/index')
+            },
+            {
+                path: 'index',
                 name: 'index',
-                component: layout,
-                children: [
-                  {
-                          path: '',
-                          name: '',
-                          meta: {
-                                  auth: true
-                          },
-                          component: _import('system/index')
-                  },
-                  {
-                          path: 'index',
-                          name: 'index',
-                          meta: {
-                                  auth: true
-                          },
-                          component: _import('system/index')
-                  }
-                ]
-        }
+                meta: {
+                    auth: true
+                },
+                component: _import('system/index')
+            },
+            {
+                path: 'show',
+                name: 'show',
+                meta: {
+                    auth: true
+                },
+                component: _import('system/index')
+            }
+        ]
+    }
 ];
 
 /**
  * 在主框架之外显示
  */
 const frameOut = [
-        {
-                path: '/login',
-                name: 'login',
-                component: _import('system/login')
-        }
+    {
+        path: '/login',
+        name: 'login',
+        component: _import('system/login')
+    }
 ];
 
 /**
@@ -51,10 +60,10 @@ const frameOut = [
 const errorPage = [];
 
 // 导出需要显示菜单的
-export const frameInRoutes = frameIn
+export const frameInRoutes = frameIn;
 
 export default [
-        ...frameIn,
-        ...frameOut,
-        ...errorPage
-]
+    ...frameIn,
+    ...frameOut,
+    ...errorPage
+];
--- examples/store/index.js
+++ examples/store/index.js
@@ -1,27 +1,27 @@
 /**
  * Created by tiner on 2019/8/27.
  */
-import Vue from 'vue'
-import Vuex from 'vuex'
+var d2admin = {
+    namespaced: true,
+    modules
+};
+
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 
-const files = require.context('./modules', false, /\.js$/)
-const modules = {}
+const files = require.context('./modules', false, /\.js$/);
+const modules = {};
 
 files.keys().forEach(key => {
-        modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
-})
-
-var d2admin = {
-        namespaced: true,
-        modules
-}
+    modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default;
+});
 
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-        modules: {
-                d2admin
-        }
-})
+    modules: {
+        d2admin
+    }
+});
